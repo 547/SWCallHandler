6952ce2c49e9a404f5b4d923b06038d6adfd7597
Seven Wang <seven.wang@supernovachina.com>
2019-06-24 09:48:19 +0800

    Initial Commit

---
+++ SWCallHandler/SWCallHandler.h
@@ -0,0 +1,19 @@
+//
+//  SWCallHandler.h
+//  SWCallHandler
+//
+//  Created by Supernova SanDick SSD on 2019/6/24.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWCallHandler.
+FOUNDATION_EXPORT double SWCallHandlerVersionNumber;
+
+//! Project version string for SWCallHandler.
+FOUNDATION_EXPORT const unsigned char SWCallHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWCallHandler/PublicHeader.h>
+
+
